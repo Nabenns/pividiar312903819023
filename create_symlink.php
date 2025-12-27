fb82bf6328c1d0889cca9ba8687d23b84c4971dd
ben <nabhanggt@gmail.com>
2025-12-28 00:58:27 +0700

    Update symlink script with debug output

--- create_symlink.php
+++ create_symlink.php
@@ -1,15 +1,30 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Starting symlink creation...\n";
+
 $target = __DIR__ . '/storage/app/public';
 $link = __DIR__ . '/public/storage';
+
+echo "Target: $target\n";
+echo "Link: $link\n";
 
 if (file_exists($link)) {
     echo "The [public/storage] link already exists.\n";
     exit;
 }
 
+if (!function_exists('symlink')) {
+    echo "Error: symlink() function is disabled.\n";
+    exit;
+}
+
 if (symlink($target, $link)) {
-    echo "The [public/storage] link has been connected to [storage/app/public].\n";
+    echo "Success: The [public/storage] link has been connected to [storage/app/public].\n";
 } else {
-    echo "The link could not be created.\n";
+    echo "Error: The link could not be created. Check permissions.\n";
+    print_r(error_get_last());
 }
