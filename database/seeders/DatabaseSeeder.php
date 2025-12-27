095d89665e6aef0b46be73d883065ac032c4198f
ben <nabhanggt@gmail.com>
2025-12-28 00:54:24 +0700

    Fix: Use User::create instead of factory in seeder to avoid faker dependency

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
 
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Admin User',
             'email' => 'admin@pividiar.com',
             'password' => bcrypt('password'),
